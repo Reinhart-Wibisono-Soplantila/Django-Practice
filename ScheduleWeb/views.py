ac135cd0a2ac81bbbe5a85cca5f2176509d96a9b
reinhart <reinhartsoplantila@gmail.com>
2024-06-01 06:55:14 +0800

    static file bootstrap

--- ScheduleWeb/views.py
+++ ScheduleWeb/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse('Hello World')
     context = {
-        'judul' : 'Kelas Terbuka',
-        'contributor' : 'Rein',
-        'subjudul' : 'Home',
+        'title' : 'Kelas Terbuka',
+        'heading' : 'Selamat Datang',
+        'subheading' : 'di Kelas Terbuka',
         'banner' : 'img/banner_home.png',
         'nav' : [
             ['/', 'Home'],

1a295d8c976ffcd07d185256e2596eacf81425a0
reinhart <reinhartsoplantila@gmail.com>
2024-06-05 14:36:53 +0800

    Field, models, options

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class post(models.Model):
+    # option max-length untuk panjang isi
+    # option blank untuk memperboleh isi kosong (default required hilang jika blank=true)
+    # option waktu auto_now_add dan fieldnya DateTimeField
     title = models.CharField(max_length=255)
     body = models.TextField()
+    email = models.EmailField(default='namaweb.com')
     
     def __str__(self):
         return"{}".format(self.title)
